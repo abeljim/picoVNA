4e088932d07a3c55ade95cc2af9752fe0311eb54
khoitd1997 <khoidinhtrinh@gmail.com>
2020-02-04 19:16:49 -0800

    make send_cmd work

--- rpi/vna_device.c
+++ rpi/vna_device.c
@@ -23,7 +23,7 @@ bool init_vna_device(VNADevice* vna)
 		/* Get the name of the port. */
 		if(SP_OK == sp_get_port_usb_vid_pid(port, &vid, &pid))
         {
-            if((VNA_VID == vid) && (VNA_VID == pid))
+            if((VNA_VID == vid) && (VNA_PID == pid))
             {
                 printf("found vna\n");
                 sp_copy_port(port, &(vna->port));
@@ -39,22 +39,36 @@ bool init_vna_device(VNADevice* vna)
         return false;
     }
 
+    sp_set_bits(vna->port, 8);
+    sp_set_stopbits(vna->port, 1);
+    sp_set_parity(vna->port, SP_PARITY_NONE);
+    sp_set_baudrate(vna->port, 9600);
+    if(SP_OK != sp_open(vna->port, SP_MODE_READ_WRITE))
+    {
+        printf("can't open vna port\n");
+        return false;
+    }
+
     return true;
 }
 
 void destroy_vna_device(VNADevice* vna)
 {
+    sp_close(vna->port);
     sp_free_port(vna->port);
 }
 
 bool send_cmd_vna_device(VNADevice* vna, const char* cmd)
 {
-    if(sp_blocking_write(vna->port, cmd, strlen(cmd) + 1, 0) < 0)
+    const size_t temp_cmd_size = strlen(cmd) + 2;
+    char* temp_cmd = (char*)malloc(temp_cmd_size);
+    snprintf(temp_cmd, temp_cmd_size - 1,"%s\r", cmd);
+    if(sp_blocking_write(vna->port, temp_cmd, temp_cmd_size, 0) < 0)
     {
         printf("error writing to vna\n");
         return false;
     }
-
+    sp_drain(vna->port);
     return true;
 }
 
